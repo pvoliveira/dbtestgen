3d16fefb02bfa48632d3cc4475fd5801a655f4f1
Paulo Vitor C de Oliveira <paulovt7@gmail.com>
2018-02-22 00:26:30 -0300

    add template to generate DDL of create table

--- src/detestgen.go
+++ src/detestgen.go
@@ -3,6 +3,8 @@ package dbtestgen
 import (
 	"database/sql"
 	"errors"
+	"strings"
+	"text/template"
 )
 
 // DBTarget - Tell us if the database is the input target or output target
@@ -16,14 +18,21 @@ const (
 )
 
 var (
-	parserDDL Parser
+	parserDDL           Parser
+	createTableTemplate = `
+CREATE TABLE {{.Schema}}.{{.Name}} (
+	{{block "listcolumns"}}	
+	{{end}}
+);`
+	columnsTableTemplate = `{{define "listcolumns"}} {{join . ", \n"}} {{end}} `
+	funcJoinString       = template.FuncMap{"join": strings.Join}
 )
 
 // Parser - defines the parser that implements queries to returns the DDL
 type Parser interface {
 	ParseColumns(db *sql.DB, schemaName, tableName string) (columnsDefinitions []sql.ColumnType, err error)
 	ParseConstraints(db *sql.DB, schemaName, tableName string) (constraintsDefinitions map[string]string, err error)
-	RawColumnType(col sql.ColumnType) (sqlType string, err error)
+	RawColumnDefinition(col sql.ColumnType) (sqlType string, err error)
 }
 
 // RegisterParser - function to register the parser according to the Driver
@@ -95,6 +104,33 @@ func recoverTableMetadata(cfg *ConfigDB) (err error) {
 }
 
 func recoverColumnsMetadata(cfg *ConfigDB, tbl *ConfigTable) ([]*ColumnMetadata, error) {
+	if parserDDL == nil {
+		panic("The parser wasn't configured. Call RegisterParser before start.")
+	}
+
+	if cfg == nil || cfg.Type != Input {
+		return nil, errors.New("Any configuration is input type")
+	}
+
+	if err := cfg.checkConn(); err != nil {
+		return nil, err
+	}
+
+	if cols, err := parserDDL.ParseColumns(cfg.DB, tbl.Schema, tbl.Name); err == nil {
+		for _, col := range cols {
+			var colDefinition string
+			colName := col.Name()
+			colDefinition, err = parserDDL.RawColumnDefinition(col)
+			tbl.Columns = append(tbl.Columns,
+				&ColumnMetadata{
+					Name:          colName,
+					RawTypeColumn: colDefinition,
+				})
+		}
+	} else {
+		return nil, err
+	}
+
 	return nil, nil
 }
 
